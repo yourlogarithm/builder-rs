ba9f3e2a6d0ef2446410fd1f8e18fdfe36f64773
Vlad-Sebastian Cretu <vcretu@bitdefender.com>
2024-03-16 15:19:02 +0200

    Fixed tests

--- tests/builder.rs
+++ tests/builder.rs
@@ -3,7 +3,7 @@ use trybuild::TestCases;
 #[test]
 fn builder() {
     let t = TestCases::new();
-    t.pass("tests/01-basic.rs");
-    t.pass("tests/02-default.rs");
-    t.compile_fail("tests/03-default-missing.rs");
+    t.pass("tests/builder/basic.rs");
+    t.pass("tests/builder/default.rs");
+    t.compile_fail("tests/builder/default-missing.rs");
 }
